b314dd4eb4ea7903c7b66fd19cfe0e1c8fb51614
tjurkiewicz <none@none>
2016-07-23 12:55:17 +0200

    Add cherrypy

---
+++ src/server.py
@@ -0,0 +1,21 @@
+import cherrypy
+
+import growbots.wsgi
+
+
+if __name__ == '__main__':
+    cherrypy.server.unsubscribe()
+
+    cherrypy.tree.graft(growbots.wsgi.application, "/")
+
+    server = cherrypy._cpserver.Server()
+
+    # might be configurable as well
+    server.socket_host = "0.0.0.0"
+    server.socket_port = 8080
+    server.thread_pool = 30
+
+    server.subscribe()
+
+    cherrypy.engine.start()
+    cherrypy.engine.block()
